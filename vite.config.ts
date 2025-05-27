20f7165378cefc557867debb30f2cc681c48ea94
Андрей Логачев <test>
2025-05-27 17:19:09 +0300

    Добавление алиасов для импортов по методологии fsd

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // @ts-expect-error for disable error declaration file for module
 import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 });
