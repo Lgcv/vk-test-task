74c4037ce611829e7b73035b0f8711d6ce85c273
Андрей Логачев <test>
2025-05-28 15:40:35 +0300

    Исправление формирования количества полей таблицы в зависимости от количества полей в полученных объектах

--- src/entities/some-entity/ui/some-entity-table.tsx
+++ src/entities/some-entity/ui/some-entity-table.tsx
@@ -11,35 +11,40 @@ import { model } from '../model';
 import { observer } from 'mobx-react-lite';
 
 export const SomeEntityTable = observer(() => {
-  const countField = 15;
   const data = model.data;
 
   return (
-    <TableContainer sx={{ maxHeight: '100%', overflow: 'auto' }} component={Paper}>
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">№</TableCell>
-            {[...Array(countField)].map((_, i) => {
-              return (
-                <TableCell key={i} sx={{ whiteSpace: 'nowrap' }} align="left">
-                  {`Поле ${i + 1}`}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, i) => (
-            <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell>{i + 1}</TableCell>
-              {Object.keys(row).map((field, i) => {
-                return <TableCell key={i}>{row[field]}</TableCell>;
-              })}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {data.length > 0 ? (
+        <TableContainer sx={{ maxHeight: '100%', overflow: 'auto' }} component={Paper}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">№</TableCell>
+                {[...Array(Object.keys(data[0]).length)].map((_, i) => {
+                  return (
+                    <TableCell key={i} sx={{ whiteSpace: 'nowrap' }} align="left">
+                      {`Поле ${i + 1}`}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row, i) => (
+                <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell>{i + 1}</TableCell>
+                  {Object.keys(row).map((field, i) => {
+                    return <TableCell key={i}>{row[field]}</TableCell>;
+                  })}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <>Данных нет</>
+      )}
+    </>
   );
 });
