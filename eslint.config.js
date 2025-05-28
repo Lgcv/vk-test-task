511ed08fd32b80980ca5a6d1badda1974307d127
Андрей Логачев <test>
2025-05-28 15:16:59 +0300

    Отключение правила eslint для rest оператора

--- eslint.config.js
+++ eslint.config.js
@@ -46,6 +46,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     },
   },
   eslintConfigPrettier,
